58ae3e2255d8671a323c3a0576583bf2cc55f493
your Name <Kosuri1992@csu.fullerton.edu>
2016-06-07 00:20:45 -0700

    updated on 06/07/2016

---
+++ Assignment2/app.js
@@ -0,0 +1,9 @@
+var a = ["1", "2", {foo:"bar"}];
+var b = a[1]; // b is now "2";
+var c = a[2]; // c now references {foo:"bar"}
+a[1] = "4";   // a is now ["1", "4", {foo:"bar"}]; b still has the value
+              // it had at the time of assignment
+a[2] = "5";   // a is now ["1", "4", "5"]; c still has the value
+              // it had at the time of assignment, i.e. a reference to
+              // the object {foo:"bar"}
+console.log(b, c.foo); // "2" "bar"
